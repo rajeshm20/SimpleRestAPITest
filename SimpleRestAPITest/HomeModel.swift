0c6dea020e28da3c02c9d46508c8ed3c3d429573
Rajesh <rajesh@Rajeshs-Mac.local>
2018-07-04 11:11:31 -0700

    first commit

---
+++ SimpleRestAPITest/HomeModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeModel.swift
+//  SimpleRestAPITest
+//
+//  Created by Rajesh on 7/4/18.
+//  Copyright © 2018 Rajesh. All rights reserved.
+//
+
+import UIKit
+
+class HomeModel: NSObject {
+
+}
---
+++ SimpleRestAPITest/LocationModel.swift
@@ -0,0 +1,13 @@
+//
+//  LocationModel.swift
+//  SimpleRestAPITest
+//
+//  Created by Rajesh on 7/4/18.
+//  Copyright © 2018 Rajesh. All rights reserved.
+//
+
+import UIKit
+
+class LocationModel: NSObject {
+
+}
